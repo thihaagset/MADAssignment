b2c818aaf953c338869f940861b354a07c12c2e0
Junjie Yan <s10185865@connect.np.edu.sg>
2019-07-27 16:13:49 +0800

    change design for user chart and login page

--- app/src/main/java/sg/edu/np/g69/madassignment/UserChart.java
+++ app/src/main/java/sg/edu/np/g69/madassignment/UserChart.java
@@ -1,12 +1,15 @@
 package sg.edu.np.g69.madassignment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,7 +38,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-public class UserChart extends AppCompatActivity {
+public class UserChart extends MainNavDrawer {
     BarChart user_Barchart;
     FirebaseFirestore db;
     FirebaseAuth user;
@@ -53,7 +56,12 @@ public class UserChart extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_chart);
+        ViewGroup root = findViewById(R.id.drawer_layout);
+        LayoutInflater inflater = (LayoutInflater) this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.activity_user_chart, root, false);
+        drawer.addView(contentView, 0);
+
         user_Barchart = findViewById(R.id.User_bar);
         ArrayList<BarEntry> user_hours_focused = new ArrayList<>();
         i = getIntent();
